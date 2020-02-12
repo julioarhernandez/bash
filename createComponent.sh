b0109f78fe6a74362f8ebd76ecf6bedd32eec9fd
Julio Rodriguez <juliorodriguez@ncl.com>
2020-02-12 11:07:19 -0500

    Working progress

--- createComponent.sh
+++ createComponent.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-STATIC_PATH="~/work/projects/oceania/redesign-oci-static/src"
-LYSSA_PATH="~/work/projects/common/oci-lyssa-mocks"
+STATIC_PATH="/Users/juliorodriguez/work/projects/oceania/redesign-oci-static/src"
+LYSSA_PATH="/Users/juliorodriguez/work/projects/common/oci-lyssa-mocks"
 
 #STATIC PATHS
 
 STATIC_SCRIPTS_MOCKS_PATH="${STATIC_PATH}/scripts/mocks"
-STATIC_SCRIPTS_PATH="${STATIC_PATH}/scripts/"
-STATIC_STYLES_PATH="${STATIC_PATH}/styles/"
+STATIC_SCRIPTS_PATH="${STATIC_PATH}/scripts"
+STATIC_STYLES_PATH="${STATIC_PATH}/styles"
 
 
 #LYSSA PATHS
-LYSSA_COMPONENTS_FOLDER="{$LYSSA_PATH}/static/oci/api/cms/v1/nclh-template/components/"
-LYSSA_MODULES_FOLDER="{$LYSSA_PATH}/static/oci/api/cms/v1/nclh-template/modules/"
-STATIC_COMPONENTS_FOLDER="{$STATIC_PATH}/static/oci/api/cms/v1/nclh-template/components/"
-STATIC_MODULES_FOLDER="{$STATIC_PATH}/static/oci/api/cms/v1/nclh-template/modules/"
+LYSSA_FOLDER="${LYSSA_PATH}/static/oci/api/cms/v1/nclh-template"
 
 LYSSA_COMPONENT_TEMPLATE="{namespace oceania.components}
 
@@ -38,30 +35,56 @@ LYSSA_MODULE_TEMPLATE="{namespace oceania.modules}
     </section>
 {/template}"
 
-STATIC_SCRIPT_COMPONENT_TEMPLATE="import $ from 'jquery';
-
+STATIC_SCRIPT_TEMPLATE="import $ from \"jquery\";\n\n
 export default \$(function() {});"
 
 STATIC_STYLE_COMPONENT_TEMPLATE=".cxxx{}"
 
+STATIC_STYLE_COMPONENT_MAIN_TEMPLATE="\n@import \"components/xxx\";"
+
 fileExist(){
   filePath=$1
-  fileName=$2
-  if [ -f $filePath ]; then
-        echo "...File doesn't exist, Let's make it"
-    else
-        echo "... File c$2.scss already exist please select another compadre, \n***** MIRA QUE YO TE LO DIGO, PRESTA MAS ATENCION ***** \n"
+  if [ -f "$filePath" ]; then
+        echo "... File ${filePath} already exist please select another compadre, \n***** MIRA QUE YO TE LO DIGO, PRESTA MAS ATENCION ***** \n"
         exit 1
+    else
+        echo "... File doesn't exist, Let's keep doing this magic"
     fi
+}
+
+createFile(){
+  filePath=$1
+  content=$2
+  echo $content > $filePath
+  echo "... Created ${filePath}"
+}
+
+appendContent(){
+  filePath=$1
+  content=$2
+  echo "${content}" >> $filePath
+}
+
+appendContentCat(){
+  filePath=$1
+  content=$2
+  echo "${content}" >> $filePath
+}
+
+replaceToken(){
+  template=$1
+  string=$2
+  echo ${template//xxx/${string}}
 }
 
 echo "
 *------------------------------------------ \n
 * What's the THING you are creating? Component[c] or Module[m]?\n"
-read THING_TYPE
+#read THING_TYPE
 #if [[ $THING_TYPE =~ ^[cC]$ ]];then
 #  TYPE='component'
 #fi
+THING_TYPE="m"
 case $THING_TYPE in
   [cC])
     TYPES='components'
@@ -84,24 +107,76 @@ echo "Your are creating a ${TYPE}"
 echo "
 *------------------------------------------ \n
 * What's the ${TYPE} number? Just a number please, no characters:\n"
-read THING_NUMBER
+#read THING_NUMBER
+THING_NUMBER="23333435"
 echo "
 * So, it's gonna be ${TYPE}: ${ABBR_TYPE}$THING_NUMBER? \n
 ----------------------------------------------------------------- \n
 Type [y] to confirm or anything else to ABORT if you chickened out\n
 -----------------------------------------------------------------\n"
-
-read confirm
+#read confirm
+confirm="y"
+ELEMENT_NAME=${ABBR_TYPE}$THING_NUMBER
 if [[ $confirm =~ ^[Yy]$ ]];then
     echo "LET'S START CREATING STATIC FILES Mijito\n"
     echo "----------------------------------------\n"
     echo "Creating styles SCSS \n"
-    STATIC_FILE="${STATIC_PATH}c$THING_NUMBER.scss";
-    MOCK_FILE=$1;
-    # make sure the file doesn't exits'
-    fileExist STATIC_FILE THING_NUMBER
 
+    # *************************************************
+    #
+    #   Creating scss
+    #
+    # *************************************************
+
+    STATIC_FILE="${STATIC_STYLES_PATH}/${TYPES}/_${ELEMENT_NAME}.scss";
+    fileExist $STATIC_FILE
+    createFile $STATIC_FILE $(replaceToken "$STATIC_STYLE_COMPONENT_TEMPLATE" $THING_NUMBER)
+
+    # *************************************************
+    #
+    #   appending main.scss styles
+    #
+    # *************************************************
+
+    MAIN_CSS_FILE="${STATIC_STYLES_PATH}/main.scss"
+    appendContent $MAIN_CSS_FILE $(replaceToken "$STATIC_STYLE_COMPONENT_MAIN_TEMPLATE" $THING_NUMBER)
+#    sed '/pattern/a some text here' filename
+
+
+    # *************************************************
+    #
+    #   creating javascript element (optional)
+    #
+    # *************************************************
+echo "
+------------------------------------------------------------------------------------------- \n
+Do you need to create a Javascript file too? Type [y] to confirm or anything else to ABORT \n
+-------------------------------------------------------------------------------------------\n"
+#read confirmJs
+confirmJs="y"
+  if [[ $confirmJs =~ ^[Yy]$ ]];then
+      #Create javascript on mocks
+      STATIC_FILE="${STATIC_SCRIPTS_MOCKS_PATH}/${TYPES}/${ELEMENT_NAME}.js";
+      fileExist $STATIC_FILE
+      echo "... Creating Javascript on ${STATIC_FILE}"
+      createFile $STATIC_FILE "$STATIC_SCRIPT_TEMPLATE" $THING_NUMBER
+      #Create javascript not in mocks
+      STATIC_FILE="${STATIC_SCRIPTS_PATH}/${TYPES}/${ELEMENT_NAME}.js";
+      fileExist $STATIC_FILE
+      echo "... Creating Javascript on ${STATIC_FILE}"
+      createFile $STATIC_FILE "$STATIC_SCRIPT_TEMPLATE" $THING_NUMBER
+  fi
 fi
-
-
-#echo $STATIC_FILE
+    echo "NOW, LET'S START CREATING LYSSA FILES\n"
+    echo "--------------------------------\n"
+    echo "Creating soy file ${TYPE}: ${ABBR_TYPE}$THING_NUMBER.soy?\n"
+    LYSSA_FILE="${LYSSA_FOLDER}/${TYPES}/oceania.${TYPES}.${ELEMENT_NAME}.soy";
+    fileExist $LYSSA_FILE
+    if [ $TYPE = 'component' ];then
+      SOY_TEMPLATE=$LYSSA_COMPONENT_TEMPLATE
+    fi
+    if [ $TYPE = 'module' ];then
+      SOY_TEMPLATE=$LYSSA_MODULE_TEMPLATE
+    fi
+    replaceToken "$SOY_TEMPLATE" $THING_NUMBER
+    createFile $LYSSA_FILE replaceToken "$SOY_TEMPLATE" $THING_NUMBER)
